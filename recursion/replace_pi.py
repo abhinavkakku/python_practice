95ddc3943e30c9a202af2985726453bbc7b19f31
Abhinav Kumar <abhinav.kakku@gmail.com>
2022-08-28 09:57:27 +0530

    recursion string replacement

--- recursion/replace_pi.py
+++ recursion/replace_pi.py
@@ -1,6 +1,16 @@
+from cgitb import small
+
+
 def replace_pi(s):
     l = len(s)
     if l == 0 or l == 1:
         return s
-    if s[si], s[si+1] == p, i:
-        smalerArray
+    if s[0] == 'p' and s[1] == 'i':
+        smallOutput = replace_pi(s[2:])
+        return '3.14' + smallOutput
+    else:
+        smallOutput = replace_pi(s[1:])
+        return s[0] + smallOutput
+
+
+print(replace_pi('apippibcd'))
